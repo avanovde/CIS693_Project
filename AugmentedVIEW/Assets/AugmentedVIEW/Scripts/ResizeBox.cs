9c6382020dfff7b3f139784ba8e564eb5ada3238
Derek VanOverloop <derek.vanoverloop@gmail.com>
2018-01-23 21:18:07 -0500

    Got a floating box to move in AR following a simulated sinusoidal pattern in the x,y,and z axes

---
+++ AugmentedVIEW/Assets/AugmentedVIEW/Scripts/ResizeBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeBox : MonoBehaviour {
+
+	private float z = 0f;
+	private float previousZ = 0f;
+	private float count = 0;
+	//private GameObject cube;
+	// Use this for initialization
+	void Start () {
+		//cube = GetComponent<GameObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		count = count + 1;
+		z = 0.5f * Mathf.Sin (count / 20); 
+		float deltaZ = z - previousZ;
+		Debug.Log ("Z: " + z + " deltaZ: " + deltaZ);
+		previousZ = z;
+		transform.Translate (new Vector3 (deltaZ, deltaZ, deltaZ));
+	}
+}
