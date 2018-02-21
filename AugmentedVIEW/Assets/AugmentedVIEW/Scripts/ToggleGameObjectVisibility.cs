1cd3dc9318e2477a5381f26231a691700b24d53a
Derek VanOverloop <derek.vanoverloop@gmail.com>
2018-02-20 21:38:44 -0500

    Set up the graph menu to toggle on an off with the plus button press

---
+++ AugmentedVIEW/Assets/AugmentedVIEW/Scripts/ToggleGameObjectVisibility.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleGameObjectVisibility : MonoBehaviour {
+
+	public GameObject ObjectToToggle;
+
+	public void ToggleObject()
+	{
+		ObjectToToggle.SetActive(!ObjectToToggle.activeSelf);
+	}
+}
